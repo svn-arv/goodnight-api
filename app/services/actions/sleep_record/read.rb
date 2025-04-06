1a34066eb48c65ef3383456f0f8dfae572f22c7f
svn-arv <sevianarivyartha@gmail.com>
2025-04-06 22:55:58 +0700

    feat: add model scoping to action read

--- app/services/actions/sleep_record/read.rb
+++ app/services/actions/sleep_record/read.rb
@@ -4,7 +4,7 @@ module Actions
       def call(filters:, scopes: [], order: { created_at: :desc }, pagination_params: nil)
         collection = ::SleepRecord.where(filters)
         scopes.each do |scope|
-          collection = collection.public_send(scope)
+          collection = collection.public_send(*scope)
         end
 
         return paginate(collection, page: pagination_params[:page], per_page: pagination_params[:per_page]) if pagination_params.present?
